d81b34d52699bc7baa1aaf2d37a4f78c1ba9ce0c
Jasmin Korat <jasminkorat204@gmail.com>
2024-01-18 15:49:05 +0530

    Modules

---
+++ src/root.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { UserModule } from './user.module';
+import { OrderModule } from './order.module';
+import { ChatModule } from './chat.module';
+
+@Module({
+  imports: [UserModule, OrderModule, ChatModule],
+  controllers: [],
+  providers: [],
+  exports:[]
+})
+
+export class RootModule {
+  constructor (){
+    console.log("Root Module")
+  }
+}
